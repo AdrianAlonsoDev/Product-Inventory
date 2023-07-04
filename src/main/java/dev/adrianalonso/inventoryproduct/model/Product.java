c7d9645902ea165e04d1de001ebf62dc1e43c45b
AdrianAlonsoDev <adrianalonsodev@outlook.com>
2023-07-04 03:00:16 +0200

    Remove unused dependencies

--- src/main/java/dev/adrianalonso/inventoryproduct/model/Product.java
+++ src/main/java/dev/adrianalonso/inventoryproduct/model/Product.java
@@ -4,7 +4,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "products")
-
 public class Product {
 	@Id
 	private String id;
