1ec7d89e0104128489fef33db27280329f3f91a7
AdrianAlonsoDev <adrianalonsodev@outlook.com>
2023-07-04 09:20:55 +0200

    Added thymeleaf /products/

---
+++ src/main/java/dev/adrianalonso/inventoryproduct/controller/view/ProductViewController.java
@@ -0,0 +1,25 @@
+package dev.adrianalonso.inventoryproduct.controller.view;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import dev.adrianalonso.inventoryproduct.model.Product;
+import dev.adrianalonso.inventoryproduct.service.ProductService;
+
+@Controller
+public class ProductViewController {
+
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping("/products")
+	public String getAllProducts(Model model) {
+		List<Product> products = productService.findAllProducts();
+		model.addAttribute("products", products);
+		return "products";
+	}
+}
